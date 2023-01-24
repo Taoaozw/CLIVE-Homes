60cfd567d3c535cd8179ee31d20c9d9b75193fe2
CLIVE <1057850893@qq.com>
2023-01-24 10:52:05 +0800

    feat: add tailwindcss dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="App bg-amber-200 text-9xl">
+            <div className="text-amber-50">11</div>
+            <div>Hello world</div>
+            <div className="font-inter">This is a good idea !</div>
+            <div>14</div>
+        </div>
+    )
+}
+
+export default App
